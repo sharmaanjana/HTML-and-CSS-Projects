17fd2be2d1dcddfcad8dda874d5f3e5f802cf3e6
sharmaanjana <sharma.anjana19@gmail.com>
2026-05-15 21:15:26 -0400

    em
    em

---
+++ Employee.cs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create first Employee object
+            Employee emp1 = new Employee();
+
+            // Assign values to first employee
+            emp1.Id = 1;
+            emp1.FirstName = "Anjana";
+            emp1.LastName = "Sharma";
+
+            // Create second Employee object
+            Employee emp2 = new Employee();
+
+            // Assign values to second employee
+            emp2.Id = 1;
+            emp2.FirstName = "John";
+            emp2.LastName = "Smith";
+
+            // Compare employees using overloaded ==
+            bool result = emp1 == emp2;
+
+            // Display result
+            Console.WriteLine("Are employees equal? " + result);
+
+            // Keep console open
+            Console.ReadLine();
+        }
+    }
+}
