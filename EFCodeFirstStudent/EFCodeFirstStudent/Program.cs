cf3512b20c4213c1bdd84c1786da88e9ece1b8e4
sharmaanjana <sharma.anjana19@gmail.com>
2026-05-20 14:37:45 -0400

    Final EF Code First Project
    Final EF Code First Project

---
+++ EFCodeFirstStudent/EFCodeFirstStudent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EFCodeFirstStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                Student student = new Student()
+                {
+                    StudentName = "Anjana"
+                };
+
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                Console.WriteLine("Student added successfully!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EFCodeFirstStudent/EFCodeFirstStudent/SchoolContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace EFCodeFirstStudent
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolDB")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
